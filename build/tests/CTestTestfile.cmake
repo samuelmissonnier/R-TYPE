549cee5ca2fd4be3956f451fb148b63615dd7df6
Missonnier <samuel.missonnier@epitech.eu>
2021-02-09 13:15:06 +0100

    less build

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuelmissonnier/tek3/B-CPP-501-NAN-5-1-rtype-killian.herbreteau/tests
-# Build directory: /home/samuelmissonnier/tek3/B-CPP-501-NAN-5-1-rtype-killian.herbreteau/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(first "unit_test")
-set_tests_properties(first PROPERTIES  _BACKTRACE_TRIPLES "/home/samuelmissonnier/tek3/B-CPP-501-NAN-5-1-rtype-killian.herbreteau/tests/CMakeLists.txt;9;add_test;/home/samuelmissonnier/tek3/B-CPP-501-NAN-5-1-rtype-killian.herbreteau/tests/CMakeLists.txt;0;")
